192867e8f28eb224d24480e9efa01df2d7f3128b
punisher077 <punisher077@users.noreply.github.com>
2018-05-31 16:14:40 -0300

    Adicionados mais 2 memes: fodase e hmm boiola. O meme n26 não aparece no app, e ele esta nos dois layouts.

--- app/src/main/java/com/jp/am/jd/pdm/memes/audio/audiomemes/MainActivity.java
+++ app/src/main/java/com/jp/am/jd/pdm/memes/audio/audiomemes/MainActivity.java
@@ -16,9 +16,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<ImageButton> butoes;
-    private int[] ids = {R.raw.acertou, R.raw.aipaipara, R.raw.bambam_birl, R.raw.borracha, R.raw.foiotimo_foiotimo, R.raw.imggeral_aimeucu, R.raw. imggeral_correberg,
+    private int[] ids = {R.raw.acertou, R.raw.aipaipara, R.raw.bambam_birl, R.raw.borracha, R.raw.foiotimo_foiotimo, R.raw.imggeral_aimeucu, R.raw.imggeral_correberg,
             R.raw.imggeral_funkingles, R.raw.papaco_aindabem, R.raw.picapau_fuitapeado, R.raw.imggeral_pauquebrando, R.raw.kidbengala_taarrombado, R.raw.kidbengala_temato, R.raw.sanguedejesus, R.raw.taldomula, R.raw.tiro,
-            R.raw.velhoaiai, R.raw.banbanfdpsai, R.raw.emorreu, R.raw.morrediabo, R.raw.ndaaver, R.raw.seubct, R.raw.soufoda, R.raw.trollei};
+            R.raw.velhoaiai, R.raw.banbanfdpsai, R.raw.emorreu, R.raw.morrediabo, R.raw.ndaaver, R.raw.seubct, R.raw.soufoda, R.raw.trollei, R.raw.fodase, R.raw.boiola};
     private MediaPlayer media;
     private int index;
     private int position;
@@ -35,33 +35,35 @@ public class MainActivity extends AppCompatActivity {
 
         butoes = new ArrayList<>();
 
-        butoes.add((ImageButton)findViewById(R.id.btn1));
-        butoes.add((ImageButton)findViewById(R.id.btn2));
-        butoes.add((ImageButton)findViewById(R.id.btn3));
-        butoes.add((ImageButton)findViewById(R.id.btn4));
-        butoes.add((ImageButton)findViewById(R.id.btn5));
-        butoes.add((ImageButton)findViewById(R.id.btn6));
-        butoes.add((ImageButton)findViewById(R.id.btn7));
-        butoes.add((ImageButton)findViewById(R.id.btn8));
-        butoes.add((ImageButton)findViewById(R.id.btn9));
-        butoes.add((ImageButton)findViewById(R.id.btn10));
-        butoes.add((ImageButton)findViewById(R.id.btn11));
-        butoes.add((ImageButton)findViewById(R.id.btn12));
-        butoes.add((ImageButton)findViewById(R.id.btn13));
-        butoes.add((ImageButton)findViewById(R.id.btn14));
-        butoes.add((ImageButton)findViewById(R.id.btn15));
-        butoes.add((ImageButton)findViewById(R.id.btn16));
-        butoes.add((ImageButton)findViewById(R.id.btn17));
-        butoes.add((ImageButton)findViewById(R.id.btn18));
-        butoes.add((ImageButton)findViewById(R.id.btn19));
-        butoes.add((ImageButton)findViewById(R.id.btn20));
-        butoes.add((ImageButton)findViewById(R.id.btn21));
-        butoes.add((ImageButton)findViewById(R.id.btn22));
-        butoes.add((ImageButton)findViewById(R.id.btn23));
-        butoes.add((ImageButton)findViewById(R.id.btn24));
+        butoes.add((ImageButton) findViewById(R.id.btn1));
+        butoes.add((ImageButton) findViewById(R.id.btn2));
+        butoes.add((ImageButton) findViewById(R.id.btn3));
+        butoes.add((ImageButton) findViewById(R.id.btn4));
+        butoes.add((ImageButton) findViewById(R.id.btn5));
+        butoes.add((ImageButton) findViewById(R.id.btn6));
+        butoes.add((ImageButton) findViewById(R.id.btn7));
+        butoes.add((ImageButton) findViewById(R.id.btn8));
+        butoes.add((ImageButton) findViewById(R.id.btn9));
+        butoes.add((ImageButton) findViewById(R.id.btn10));
+        butoes.add((ImageButton) findViewById(R.id.btn11));
+        butoes.add((ImageButton) findViewById(R.id.btn12));
+        butoes.add((ImageButton) findViewById(R.id.btn13));
+        butoes.add((ImageButton) findViewById(R.id.btn14));
+        butoes.add((ImageButton) findViewById(R.id.btn15));
+        butoes.add((ImageButton) findViewById(R.id.btn16));
+        butoes.add((ImageButton) findViewById(R.id.btn17));
+        butoes.add((ImageButton) findViewById(R.id.btn18));
+        butoes.add((ImageButton) findViewById(R.id.btn19));
+        butoes.add((ImageButton) findViewById(R.id.btn20));
+        butoes.add((ImageButton) findViewById(R.id.btn21));
+        butoes.add((ImageButton) findViewById(R.id.btn22));
+        butoes.add((ImageButton) findViewById(R.id.btn23));
+        butoes.add((ImageButton) findViewById(R.id.btn24));
+        butoes.add((ImageButton) findViewById(R.id.btn25));
+        butoes.add((ImageButton) findViewById(R.id.btn26));
 
 
-        for (int i = 0; i < butoes.size(); i++){
+        for (int i = 0; i < butoes.size(); i++) {
             Glide.with(getApplicationContext()).load(R.drawable.green).into(butoes.get(i));
         }
 
@@ -83,18 +85,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        for (final ImageButton btn: butoes){
+        for (final ImageButton btn : butoes) {
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     try {
-                        if (media.isPlaying()){
+                        if (media.isPlaying()) {
                             Glide.with(getApplicationContext()).load(R.drawable.green).into(butoes.get(index));
                             media.stop();
                             media.reset();
                         }
                         index = butoes.indexOf(btn);
-                        media =  MediaPlayer.create(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + ids[index]));
+                        media = MediaPlayer.create(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + ids[index]));
                         Glide.with(getApplicationContext()).load(R.drawable.red).into(btn);
                         media.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                             @Override
@@ -103,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                         media.start();
-                    } catch (Exception e){
+                    } catch (Exception e) {
                         //faz nada
                     }
                 }
@@ -113,18 +115,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
-        if (media.isPlaying()){
+        if (media.isPlaying()) {
             media.stop();
             media.reset();
         }
     }
 
     @Override
-    public void onStop(){
+    public void onStop() {
         super.onStop();
-        if (media.isPlaying()){
+        if (media.isPlaying()) {
             position = media.getCurrentPosition();
             media.stop();
             media.reset();
@@ -134,7 +136,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRestart(){
+    public void onRestart() {
         super.onRestart();
         if (position != -1) {
             media = MediaPlayer.create(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + ids[index]));
